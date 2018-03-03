ae88dd6cfb4cae23c19ad45b791374a3707eb778
ergenekonyigit <yigitergenekon@gmail.com>
2018-03-03 14:00:34 +0300

    Homogeneity Of Variance tests added

--- src/HomogeneityOfVariance/BrownForsythe.js
+++ src/HomogeneityOfVariance/BrownForsythe.js
@@ -1,3 +1,42 @@
-export default function BrownForsythe() {
+import { sum, mean, median } from 'simple-statistics';
+import { reduceDigit } from '../util';
 
+export default function BrownForsythe({ observed, alpha = 0.05, digit = 4, way = 'one-way' }) {
+	const c = observed.length;
+	const flatObserved = observed.flatten();
+	const n = flatObserved.length;
+	const ni = Array.from({ length: c }, (_, i) => observed[i].length)
+	const medianObserved = Array.from({ length: c }, (_, i) => median(observed[i]));
+	const Z = Array.from({ length: c }, (_, i) => {
+		return Array.from({ length: ni[i] }, (_, j) => Math.abs(observed[i][j] - medianObserved[i]));
+	});
+	const sumZi = Z.map((_, i) => sum(Z[i]));
+	const meanZi = sumZi.map((v, i) => v / ni[i]);
+	const meanZ = sum(sumZi) / n;
+	const SSB = sum(Array.from({ length: c }, (_, i) => ni[i] * ((meanZi[i] - meanZ) ** 2)))
+	const SSW = sum(Array.from({ length: c }, (_, i) => {
+		return sum(Array.from({ length: ni[i] }, (_, j) => (Z[i][j] - meanZi[i]) ** 2));
+	}));
+	const MSB = SSB / (c - 1);
+	const MSW = SSW / (n - c);
+	const fCalc = MSB / MSW;
+	// const fTable = F(k - 1, n - k, alpha);
+	return {
+		c,
+		n,
+		ni,
+		observed,
+		flatObserved,
+		medianObserved,
+		Z,
+		sumZi,
+		meanZi,
+		meanZ,
+		SSB: reduceDigit(SSB, digit),
+		SSW: reduceDigit(SSW, digit),
+		MSB: reduceDigit(MSB, digit),
+		MSW: reduceDigit(MSW, digit),
+		fCalc: reduceDigit(fCalc, digit)
+		// fTable
+	}
 }
--- src/HomogeneityOfVariance/LayardChiSquare.js
+++ src/HomogeneityOfVariance/LayardChiSquare.js
@@ -1,3 +1,49 @@
-export default function LayardChiSquare() {
+import { sum, mean, chiSquaredDistributionTable } from 'simple-statistics';
+import { reduceDigit } from '../util';
 
+export default function LayardChiSquare({ observed, alpha = 0.05, digit = 4, way = 'one-way' }) {
+	const c = observed.length;
+	const df = c - 1;
+	const flatObserved = observed.flatten();
+	const n = flatObserved.length;
+	const ni = Array.from({ length: c }, (_, i) => observed[i].length)
+	const meanObserved = Array.from({ length: c }, (_, i) => mean(observed[i]));
+	const Z = Array.from({ length: c }, (_, i) => {
+		return Array.from({ length: ni[i] }, (_, j) => observed[i][j] - meanObserved[i]);
+	});
+	const SSW = Array.from({ length: c }, (_, i) => {
+		return sum(Array.from({ length: ni[i] }, (_, j) => Z[i][j] ** 2));
+	});
+	const SSSW = Array.from({ length: c }, (_, i) => {
+		return sum(Array.from({ length: ni[i] }, (_, j) => Z[i][j] ** 4));
+	});
+	const gamma = ((n * sum(SSSW)) / (sum(SSW) ** 2)) - 3;
+	const T = 2 + (1 - (c / n)) * gamma;
+	const sSqr = Array.from({ length: c }, (_, i) => SSW[i] / c);
+	const logsSqr = Array.from({ length: c }, (_, i) => Math.log10(SSW[i] / c));
+	const calc1 = sum(Array.from({ length: c }, (_, i) => ((ni[i] - 1) * logsSqr[i]))) /
+		sum(Array.from({ length: c }, (_, i) => ni[i] - 1));
+	const calc2 = Array.from({ length: c }, (_, i) => ((ni[i] - 1) * ((logsSqr[i] - calc1) ** 2)));
+	const L = sum(calc2) / T;
+	const chiSqrTable = (df, alpha) => chiSquaredDistributionTable[df][alpha];
+
+	return {
+		c,
+		df,
+		n,
+		ni,
+		observed,
+		meanObserved,
+		Z,
+		SSW,
+		SSSW,
+		gamma: reduceDigit(gamma, digit),
+		T: reduceDigit(T, digit),
+		sSqr: reduceDigit(sSqr, digit),
+		logsSqr: reduceDigit(logsSqr, digit),
+		calc1: reduceDigit(calc1, digit),
+		calc2: reduceDigit(calc2, digit),
+		L: reduceDigit(L, digit),
+		chiSqrTable: chiSqrTable(df, alpha)
+	}
 }
