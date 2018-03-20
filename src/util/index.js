27861573d99757de89823bff592f45abc3ddeb6f
ergenekonyigit <yigitergenekon@gmail.com>
2018-03-20 23:32:01 +0300

    sortArr method

--- src/util/index.js
+++ src/util/index.js
@@ -10,3 +10,5 @@ export const flatten = (arr, depth = 1) =>
   depth !== 1 ?
     arr.reduce((a, v) => a.concat(Array.isArray(v) ? flatten(v, depth - 1) : v), []) :
     arr.reduce((a, v) => a.concat(v), []);
+
+export const sortArr = f => arr => [...arr].sort(f);
