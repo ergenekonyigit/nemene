1ce924c9b7da10f4f3c7b43782cc5b2f1aeae375
ergenekonyigit <yigitergenekon@gmail.com>
2018-03-20 23:35:57 +0300

    Two Independent Samples

--- src/TwoIndependentSamples/ScaleProblem/Mood.js
+++ src/TwoIndependentSamples/ScaleProblem/Mood.js
@@ -1,3 +1,51 @@
-export default function Mood() {
+/* eslint no-nested-ternary: 0 no-return-assign: 0 */
+import { sum, mean } from 'simple-statistics';
+import { sortArr, flatten } from './util';
 
+export default function Mood({ observed, alpha = 0.05, digit = 4, way = 'one-way' }) {
+  const x = observed[0];
+  const y = observed[1];
+  const flattenObserved = flatten(observed);
+  const n1 = x.length;
+  const n2 = y.length;
+  const n = n1 + n2;
+  const indexedObs = flattenObserved.map((v, i) => ({ value: v, index: i }));
+  const indexSortedObs = sortArr((x, y) => x.value > y.value ? 1 : x.value === y.value ? 0 : -1)([...indexedObs]);
+  const values = [...new Set(indexSortedObs.map(v => v.value))];
+  let meanIndice = [];
+
+  values.forEach((item, i) => {
+    const indexes = indexSortedObs.map((item, i) => {
+      item.index = i;
+      return item;
+    })
+      .filter(a => a.value === item)
+      .map(a => a.index + 1);
+
+    meanIndice[i] = indexes.map(x => mean(indexes));
+  });
+  const flatMeanIndice = meanIndice.flatten();
+
+  indexSortedObs.map((v, i) => v.index = flatMeanIndice[i]);
+  const rObs = indexedObs.map(x => x.index);
+  const rX = Array.from({ length: n1 }, (_, i) => rObs[i]);
+  const rY = Array.from({ length: n2 }, (_, i) => rObs[n1 + i]);
+  const ri = rX;
+  const riMinus = ri.map(v => v - ((n + 1) / 2));
+  const riMinusSqr = riMinus.map(v => v ** 2);
+  const M = sum(riMinusSqr);
+
+  return {
+    x,
+    y,
+    n1,
+    n2,
+    n,
+    rX,
+    rY,
+    ri,
+    riMinus,
+    riMinusSqr,
+    M
+  };
 }
--- src/TwoIndependentSamples/ScaleProblem/SiegelTukey.js
+++ src/TwoIndependentSamples/ScaleProblem/SiegelTukey.js
@@ -1,3 +1,41 @@
-export default function SiegelTukey() {
+/* eslint no-nested-ternary: 0 consistent-return: 0*/
+import { sum } from 'simple-statistics';
+import { sortArr, flatten } from './util';
 
+export default function SiegelTukey({ observed, alpha = 0.05, digit = 4, way = 'one-way' }) {
+  const x = observed[0];
+  const xObj = x.map(v => ({ value: v, index: 'X' }));
+  const y = observed[1];
+  const yObj = y.map(v => ({ value: v, index: 'Y' }));
+  const flattenObserved = flatten(observed);
+  const i = flattenObserved.map((_, i) => i + 1);
+  const n1 = x.length;
+  const n2 = y.length;
+  const n = n1 + n2;
+  const xy = [...xObj, ...yObj];
+  const sortedXY = sortArr((_x, _y) => _x.value > _y.value ? 1 : _x.value === _y.value ? 0 : -1)([...xy]);
+  const sortedX = sortedXY.map(v => v.value);
+  const sortedY = sortedXY.map(v => v.index);
+  const delta = sortedXY.map(v => v.index === 'X' ? 1 : 0);
+  const ai = i.map((v, i) => {
+    if (v % 2 === 0 && v > 1 && v <= (n / 2)) return 2 * v;
+    if ((v % 2 !== 0) && (v >= 1) && (v <= (n / 2))) return 2 * v - 1;
+    if (v % 2 === 0 && v > (n / 2) && v <= n) return 2 * (n - v) + 2;
+    if (v % 2 !== 0 && v > (n / 2) && v < n) return 2 * (n - v) + 1;
+  });
+  const ST = sum(ai.map((v, i) => v * delta[i]));
+
+  return {
+    x,
+    y,
+    n1,
+    n2,
+    n,
+    i,
+    ai,
+    sortedX,
+    sortedY,
+    delta,
+    ST
+  };
 }
