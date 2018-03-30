6c1cc974bb8d75db272d6d212be88b6c067c807b
ergenekonyigit <yigitergenekon@gmail.com>
2018-03-30 15:00:55 +0300

    N Independent Samples

--- src/NIndependentSamples/KruskalWallis.js
+++ src/NIndependentSamples/KruskalWallis.js
@@ -1,3 +1,54 @@
-export default function KruskalWallis() {
+/* eslint no-return-assign: 0 */
+import { sum, mean } from 'simple-statistics';
+import { clone, frequency, head, shift, sortArr, flatten } from './util';
 
+export default function KruskalWallis({ observed, alpha = 0.05, digit = 4 }) {
+  const ni = observed.map(v => v.length);
+  const n = sum(ni);
+  const flattenObserved = flatten(observed);
+  const indexedObs = flattenObserved.map((v, i) => ({ value: v, index: i }));
+  const indexSortedObs = sortArr((x, y) => x.value > y.value ? 1 : x.value === y.value ? 0 : -1)([...indexedObs]);
+  const values = [...new Set(indexSortedObs.map(v => v.value))];
+  const meanIndice = [];
+
+  values.forEach((item, i) => {
+    const indexes = indexSortedObs.map((val, j) => {
+      val.index = j;
+      return val;
+    })
+      .filter(a => a.value === item)
+      .map(a => a.index + 1);
+
+    meanIndice[i] = indexes.map(x => mean(indexes));
+  });
+  const flatMeanIndice = flatten(meanIndice);
+
+  indexSortedObs.map((v, i) => v.index = flatMeanIndice[i]);
+  let rr = indexedObs.map(v => v.index);
+  const clonedObs = clone(observed);
+  const rObs = clonedObs.map(v => v.map(val => {
+    val = head(rr);
+    rr = shift(rr);
+    return val;
+  }));
+  const Rij = rObs.map(v => sum(v));
+  const H = (12 / (n * (n + 1))) * sum(Rij.map((v, i) => (v ** 2) / ni[i])) - (3 * (n + 1));
+  const frequencyRank = frequency(flatMeanIndice);
+  const frequencyArr = [...frequencyRank.values()];
+  const Ti = frequencyArr.map(v => (v - 1) * v * (v + 1));
+  const HStar = H / (1 - (sum(Ti) / ((n ** 3) - n)));
+
+  return {
+    ni,
+    n,
+    observed,
+    flattenObserved,
+    flatMeanIndice,
+    rObs,
+    Rij,
+    H,
+    frequencyArr,
+    Ti,
+    HStar
+  };
 }
--- src/NIndependentSamples/Median.js
+++ src/NIndependentSamples/Median.js
@@ -1,3 +1,35 @@
-export default function Median() {
+import { sum, median } from 'simple-statistics';
+import { sortArr, flatten } from '../util';
 
+export default function Median({ observed, alpha = 0.05, digit = 4 }) {
+  const ni = observed.map(v => v.length);
+  const n = sum(ni);
+  const flattenObserved = flatten(observed);
+  const m = median(flattenObserved);
+  const sortedObserved = observed.map(v => sortArr((a, b) => a - b)(v));
+  const nMedian = Array.from({ length: ni.length }, () => [[], []]);
+
+  observed.map((v, i) => v.map((val, j) => val > m ? nMedian[i][0].push(val) : nMedian[i][1].push(val)));
+  const nMedianLength = nMedian.map(v => v.map(val => val.length));
+  const sumI = Array.from({ length: nMedianLength[0].length }, (_, i) => sum(nMedianLength.map(v => v[i])));
+  const sumJ = Array.from({ length: nMedianLength.length }, (_, i) => sum(nMedianLength[i]));
+  const expected = Array.from({ length: sumJ.length }, (_, i) =>
+    Array.from({ length: sumI.length }, (_, j) => sumJ[i] / 2));
+  const chiSqr = expected.map((v, i) => v.map((val, j) => (nMedianLength[i][j] - val) ** 2 / val));
+  const chiSqrCalc = sum(chiSqr.map(v => sum(v)));
+
+  return {
+    ni,
+    n,
+    m,
+    observed,
+    sortedObserved,
+    nMedian,
+    nMedianLength,
+    sumI,
+    sumJ,
+    expected,
+    chiSqr,
+    chiSqrCalc
+  };
 }
