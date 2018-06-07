d41986d98267db64a46fa36d80e83f271d535e73
ergenekonyigit <yigitergenekon@gmail.com>
2018-06-07 23:23:07 +0300

    minor improvements

--- src/index.js
+++ src/index.js
@@ -1,48 +1,80 @@
 // Tests of Goodness of Fit
-export { ChiSquareGoodnessOfFit } from './GoodnessOfFit/ChiSquareGoodnessOfFit';
-export { KolmogorovSmirnovGoodnessOfFit } from './GoodnessOfFit/KolmogorovSmirnovGoodnessOfFit';
-export { Lilliefors } from './GoodnessOfFit/Lilliefors';
-export { ShapiroWilk } from './GoodnessOfFit/ShapiroWilk';
+import ChiSquareGoodnessOfFit from './GoodnessOfFit/ChiSquareGoodnessOfFit';
+import KolmogorovSmirnovGoodnessOfFit from './GoodnessOfFit/KolmogorovSmirnovGoodnessOfFit';
+import Lilliefors from './GoodnessOfFit/Lilliefors';
+import ShapiroWilk from './GoodnessOfFit/ShapiroWilk';
 
 // Tests for Homogeneity of Variance
-export { Levene } from './HomogeneityOfVariance/Levene';
-export { BrownForsythe } from './HomogeneityOfVariance/BrownForsythe';
-export { LayardChiSquare } from './HomogeneityOfVariance/LayardChiSquare';
+import Levene from './HomogeneityOfVariance/Levene';
+import BrownForsythe from './HomogeneityOfVariance/BrownForsythe';
+import LayardChiSquare from './HomogeneityOfVariance/LayardChiSquare';
 
 // Tests for One Sample
-export { Binomial } from './OneSample/Binomial';
-export { OneSampleSign } from './OneSample/OneSampleSign';
-export { WilcoxonSignedRanks } from './OneSample/WilcoxonSignedRanks';
-export { Runs } from './OneSample/Runs';
+import Binomial from './OneSample/Binomial';
+import OneSampleSign from './OneSample/OneSampleSign';
+import WilcoxonSignedRanks from './OneSample/WilcoxonSignedRanks';
+import Runs from './OneSample/Runs';
 
 // Tests for Two Independent Samples
 // - Location Problem
-export { LocationMedian } from './TwoIndependentSamples/LocationProblem/LocationMedian';
-export { MannWhitney } from './TwoIndependentSamples/LocationProblem/MannWhitney';
-export { WaldWolfowitz } from './TwoIndependentSamples/LocationProblem/WaldWolfowitz';
-export { KolmogorovSmirnov } from './TwoIndependentSamples/LocationProblem/KolmogorovSmirnov';
-export { FishersExact } from './TwoIndependentSamples/LocationProblem/FishersExact';
+import LocationMedian from './TwoIndependentSamples/LocationProblem/LocationMedian';
+import MannWhitney from './TwoIndependentSamples/LocationProblem/MannWhitney';
+import WaldWolfowitz from './TwoIndependentSamples/LocationProblem/WaldWolfowitz';
+import KolmogorovSmirnov from './TwoIndependentSamples/LocationProblem/KolmogorovSmirnov';
+import FishersExact from './TwoIndependentSamples/LocationProblem/FishersExact';
 // - Scale Problem
-export { Mood } from './TwoIndependentSamples/ScaleProblem/Mood';
-export { SiegelTukey } from './TwoIndependentSamples/ScaleProblem/SiegelTukey';
+import Mood from './TwoIndependentSamples/ScaleProblem/Mood';
+import SiegelTukey from './TwoIndependentSamples/ScaleProblem/SiegelTukey';
 
 // Tests for Two Paired Samples
-export { Sign } from './TwoPairedSamples/Sign';
-export { WilcoxonRankSum } from './TwoPairedSamples/WilcoxonRankSum';
-export { McNemar } from './TwoPairedSamples/McNemar';
+import Sign from './TwoPairedSamples/Sign';
+import WilcoxonRankSum from './TwoPairedSamples/WilcoxonRankSum';
+import McNemar from './TwoPairedSamples/McNemar';
 
 // Chi Square Tests for Homogeneity and Independence
-export { ChiSquareForHomogeneity } from './ChiSquareTests/ChiSquareForHomogeneity';
-export { ChiSquareForIndependence } from './ChiSquareTests/ChiSquareForIndependence';
+import ChiSquareForHomogeneity from './ChiSquareTests/ChiSquareForHomogeneity';
+import ChiSquareForIndependence from './ChiSquareTests/ChiSquareForIndependence';
 
 // Tests for n Independent Samples
-export { Median } from './NIndependentSamples/Median';
-export { KruskalWallis } from './NIndependentSamples/KruskalWallis';
+import Median from './NIndependentSamples/Median';
+import KruskalWallis from './NIndependentSamples/KruskalWallis';
 
 // Tests for n Paired Samples
-export { FriedmansS } from './NPairedSamples/FriedmansS';
-export { CochransQ } from './NPairedSamples/CochransQ';
+import FriedmansS from './NPairedSamples/FriedmansS';
+import CochransQ from './NPairedSamples/CochransQ';
 
 // Correlation
-export { SpearmanRankCorrelation } from './Correlation/SpearmanRankCorrelation';
-export { Kendall } from './Correlation/Kendall';
+import SpearmanRankCorrelation from './Correlation/SpearmanRankCorrelation';
+import Kendall from './Correlation/Kendall';
+
+export {
+  ChiSquareGoodnessOfFit,
+  KolmogorovSmirnovGoodnessOfFit,
+  Lilliefors,
+  ShapiroWilk,
+  Levene,
+  BrownForsythe,
+  LayardChiSquare,
+  Binomial,
+  OneSampleSign,
+  WilcoxonSignedRanks,
+  Runs,
+  LocationMedian,
+  MannWhitney,
+  WaldWolfowitz,
+  KolmogorovSmirnov,
+  FishersExact,
+  Mood,
+  SiegelTukey,
+  Sign,
+  WilcoxonRankSum,
+  McNemar,
+  ChiSquareForHomogeneity,
+  ChiSquareForIndependence,
+  Median,
+  KruskalWallis,
+  FriedmansS,
+  CochransQ,
+  SpearmanRankCorrelation,
+  Kendall
+};
